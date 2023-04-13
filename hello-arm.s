64e68f50a368b9b15451a7cbed23d914b63f6db5
Peter Yoon <peyo.hd@gmail.com>
2023-04-13 16:18:49 +0900

    add hello-arm.s
     $ clang -target armv7a-linux-androideabi -c -nostdlib -o hello.o hello-arm.s
     $ ld.lld -o hello hello.o
     $ adb push hello /data/
     $ adb shell
    
     # strace /data/hello

---
+++ hello-arm.s
@@ -0,0 +1,17 @@
+.text
+.globl _start
+
+_start:
+    mov     r0, #1				// file descriptor number 1 (stdout)
+    ldr     r1, =message
+    mov     r2, #7
+    mov     r7, #4				// syscall 4 (write)
+    swi     #0
+
+    mov     r0, #0				// exit status 0 (ok)
+    mov     r7, #1				// syscall 1 (exit)
+    swi     #0
+	
+.data
+message:
+    .ascii      "\nhello\n"
