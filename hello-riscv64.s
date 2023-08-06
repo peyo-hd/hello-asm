0db3f5a36bdc16994ff07c9919bc1dbf27944a9b
Peter Yoon <peyo.hd@gmail.com>
2023-08-06 13:34:07 +0900

    test add x9,x20,x21

--- hello-riscv64.s
+++ hello-riscv64.s
@@ -2,17 +2,11 @@
 .globl _start
 
 _start:	
-    li     a0, 1				// file descriptor number 1 (stdout)
-    la     a1, message
-    li     a2, 7
-    li     a7, 64				// syscall 64 (write)
-    ecall
+	add x9, x20, x21
 
     li     a0, 0				// exit status 0 (ok)
     li     a7, 93				// syscall 93 (exit)
     ecall
 	
-.data
-message:
-    .ascii      "\nhello\n"
+
 
