2ac8033895a45cd268e6816d09aac93bcb71a8c7
Peter Yoon <peyo.hd@gmail.com>
2023-08-08 20:48:22 +0900

    float matrix multiply

--- hello-riscv64.c
+++ hello-riscv64.c
@@ -1,3 +1,7 @@
-float _start(float fahr) {
-  return ((5.0/9.0) * (fahr-32.0));
+void _start(float c[32][32], float a[32][32], float b[32][32]) {
+  int i,j,k;
+  for(i=0;i<32;i++)
+    for(j=0;j<32;j++)
+      for(k=0;k<32;k++)
+	c[i][j]=c[i][j]+a[i][k]*b[k][j];
 }
